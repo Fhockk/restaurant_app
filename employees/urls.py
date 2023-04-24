f51b6a7ddb2e747653fd78c23c92478285c4e94b
Anatolii Khyzun <anatoliy.fhock@gmail.com>
2023-04-25 02:29:59 +0300

    update readme.md

--- employees/urls.py
+++ employees/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]

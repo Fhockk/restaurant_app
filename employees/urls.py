74d5248414ae4e10ce0a20042ebfe427263bbcbf
Anatolii Khyzun <anatoliy.fhock@gmail.com>
2023-04-24 18:58:46 +0300

    simple-jwt token auth

--- employees/urls.py
+++ employees/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView, TokenVerifyView,
+)
+
 from employees.views import EmployeeModelViewSet
+
 
 router = routers.SimpleRouter()
 router.register(r'employees', EmployeeModelViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
